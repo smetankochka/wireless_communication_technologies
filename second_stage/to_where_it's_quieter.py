8be39d43fd7e5aea702344823eb10104812d314a
smetankochk <smetanochka@petalmail.com>
2023-11-18 00:25:54 +0700

    add answer

--- second_stage/to_where_it's_quieter.py
+++ second_stage/to_where_it's_quieter.py
@@ -1,3 +1,12 @@
 import sys
 
-data = list(map(str.strip, sys.stdin))
+data = [x.split("\t") for x in list(map(str.strip, sys.stdin))[1:]]
+channels = [0] * 28
+for _, channel, percent in data:
+    channels[int(channel)] += float(percent)
+ans, minimal = -1, 1
+for channel, percent in enumerate(channels):
+    if minimal > percent:   # если рассматриваем только используемые, то ...and percent != 0
+        minimal = percent
+        ans = channel
+print(ans)
