538a8af4063833dbe9e3af3d24bee0a19d31c889
smetanka <mmura997@yandex.by>
2023-11-21 17:17:23 +0700

    update optometry(false)

--- second_stage/incorrect_optometry.py
+++ second_stage/incorrect_optometry.py
@@ -1,5 +1,14 @@
 import sys
+import math
 
 data = [x.split() for x in list(map(str.strip, sys.stdin))]
+sums = 0
 for i, cor in enumerate(data):
-    data[i] = [int(cor[0]) / 1000, int(cor[1], 16)]
+    inf = len(bin(int(cor[1], 16))[2:])
+    sums += inf
+    data[i] = tuple([int(cor[0]) / 1000, inf])
+data = sorted(data)
+start, finish = data[0][0], data[-1][0]
+print(start, finish)
+t = finish - start
+print(math.floor(sums / t))
