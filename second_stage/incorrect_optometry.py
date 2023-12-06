b6ed2674c49802b6050abdbbd3142cb07091d118
$m3ť@nk@ <148964894+smetankochka@users.noreply.github.com>
2023-12-06 23:20:38 +0700

    Update incorrect_optometry.py

--- second_stage/incorrect_optometry.py
+++ second_stage/incorrect_optometry.py
@@ -2,13 +2,17 @@ import sys
 import math
 
 data = [x.split() for x in list(map(str.strip, sys.stdin))]
-sums = 0
+time = [0] * len(data)
+length = [0] * len(data)
+mintime = 1e18
+maxtime = -1e18
+infall = 0
 for i, cor in enumerate(data):
-    inf = len(bin(int(cor[1], 16))[2:])
-    sums += inf
-    data[i] = tuple([int(cor[0]) / 1000, inf])
-data = sorted(data)
-start, finish = data[0][0], data[-1][0]
-print(start, finish)
-t = finish - start
-print(math.floor(sums / t))
+    inform = int(cor[1][8:12], 16) * 8 + 64 + 160
+    infall += inform
+    timenow = int(cor[0]) / 1000
+    if timenow < mintime:
+        mintime = timenow
+    if timenow > maxtime:
+        maxtime = timenow
+print(math.floor(infall / (maxtime - mintime)))
