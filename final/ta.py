0b56cf2398687b73a7eb50f86aecb3a988021ac6
smetanka <smetankochka@petalmail.com>
2024-02-27 23:19:54 +0300

    add some from tbs

---
+++ final/ta.py
@@ -0,0 +1,38 @@
+import csv
+import matplotlib.pyplot as plt
+
+x, y = [], []
+with open('ta1.csv', 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    f = True
+    for row in csvreader:
+        if f:
+            f = False
+            continue
+        x.append(float(row[0]))
+        if (zn := float(row[2])) > 2750:
+            y.append(zn)
+        else:
+            y.append(0)
+
+
+for i in range(0, 6000, 375):
+    chunkx = x[i:i + 375]
+    chunky = y[i:i + 375]
+    toplet = 2750
+
+    # plt.plot(chunkx, chunky)
+    # plt.title('Plot of CSV')
+    # plt.xlabel('X')
+    # plt.ylabel('Y')
+    # plt.show()
+
+    flag = False
+    for j in range(100):
+        if chunky[j] > toplet:
+            flag = True
+            break
+    if flag:
+        print(1, end="")
+    else:
+        print(0, end="")
